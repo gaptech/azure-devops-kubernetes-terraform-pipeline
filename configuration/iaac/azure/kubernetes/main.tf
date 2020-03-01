14362b58329e84c4b0bc97224fb33ea3d2a33b96
gaptech <60424109+gaptech@users.noreply.github.com>
2020-03-01 19:42:57 +0530

    Update main.tf
    features

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -9,6 +9,9 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   resource_group_name = azurerm_resource_group.resource_group.name
   dns_prefix          = var.dns_prefix
 
+  features{
+  }
+  
   linux_profile {
     admin_username = "ubuntu"
 
