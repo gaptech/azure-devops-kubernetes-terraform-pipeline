3ba46d5da1f8219c77d8cd6ea7db0ddcbc64df4c
gaptech <60424109+gaptech@users.noreply.github.com>
2020-03-01 19:49:55 +0530

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+   features {}
+}
+
 resource "azurerm_resource_group" "resource_group" {
   name     = "${var.resource_group}_${var.environment}"
   location = var.location
@@ -41,7 +45,4 @@ terraform {
     # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
     # container_name="<<storage_account_container_name>>" #OVERRIDE in TERRAFORM init
   }
-  
-  features{
-    }
 }
